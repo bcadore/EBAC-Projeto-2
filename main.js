989b638e2c44555062e42e68767cfb8f19677e3f
Bernardo Cadore Ramires <bernardo.cadore95@gmail.com>
2024-02-16 00:37:14 -0300

    Modificações nos arquivos

--- main.js
+++ main.js
@@ -1,28 +1,61 @@
 const form = document.getElementById("form-atividade");
-const imgAprovado = `<img src= "./Assets/aprovado.png" alt="Emoji celebrando" /> `;
-const imgReprovado = `<img src= "./Assets/reprovado.png" alt="Emoji chorando" /> `;
+const imgAprovado =
+  '<img src= "./Assets/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado =
+  '<img src= "./Assets/reprovado.png" alt="Emoji chorando" />';
+const atividades = [];
+const notas = [];
 
-
-let linhas = ``;
+let linhas = "";
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const inputNomeAtividade = document.getElementById("nome-atividade");
-  const inputNotaATividade = document.getElementById("nota-atividade");
-
-  let linha = `<tr>`;
-  linha += `<td>${inputNomeAtividade.value}</td>`;
-  linha += `<td>${inputNotaATividade.value}</td>`;
-  linha += `<td>${inputNotaATividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
-  linha += `</tr>`;
-
-  linhas += linha; 
-
-  const corpoTabela = document.querySelector('tbody');
-  corpoTabela.innerHTML = linhas;
-
-  inputNomeAtividade = ``;
-  inputNotaATividade = ``;
-
+  adicionaLinha();
+  atualizaTabela();
+  atualizaMediaFinal();
 });
+
+function adicionaLinha() {
+  const inputNomeAtividade = document.getElementById("nome-atividade");
+  const inputNotaAtividade = document.getElementById("nota-atividade");
+
+  atividades.push(inputNomeAtividade.value);
+  notas.push(parseFloat(inputNotaAtividade.value));
+
+  let linha = "<tr>";
+  linha += `<td>${inputNomeAtividade.value}</td>`;
+  linha += `<td>${inputNotaAtividade.value}</td>`;
+  linha += `<td>${
+    inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado
+  }</td>`;
+  linha += "</tr>";
+
+  linhas += linha;
+
+  inputNomeAtividade.value = "";
+  inputNotaAtividade.value = "";
+}
+
+function atualizaTabela() {
+  const corpoTabela = document.querySelector("tbody");
+  corpoTabela.innerHTML = linhas;
+}
+
+function atualizaMediaFinal() {
+  const mediaFinal = calculaMediaFinal();
+
+  document.getElementById("media-final-valor").innerHTML = mediaFinal;
+  document.getElementById("media-final-resultado").innerHTML =
+    mediaFinal >= 7 ? "Aprovado" : "Reprovado";
+}
+
+function calculaMediaFinal() {
+  let somaDasNotas = 0;
+
+  for (i = 0; i < notas.length; i++) {
+    somaDasNotas += notas[i];
+  }
+
+  return somaDasNotas / notas.length;
+}
