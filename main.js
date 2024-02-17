18e59c7426b0db095cf46be6cd2316eec264e8c4
Bernardo Cadore Ramires <bernardo.cadore95@gmail.com>
2024-02-16 21:38:49 -0300

    Alterado arquivos para atualizar o cálculo da média.

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const imgReprovado =
   '<img src= "./Assets/reprovado.png" alt="Emoji chorando" />';
 const atividades = [];
 const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+const notaMinima = parseFloat(prompt("Digite a nota mínima: "));
 
 let linhas = "";
 
@@ -20,18 +23,22 @@ function adicionaLinha() {
   const inputNomeAtividade = document.getElementById("nome-atividade");
   const inputNotaAtividade = document.getElementById("nota-atividade");
 
-  atividades.push(inputNomeAtividade.value);
-  notas.push(parseFloat(inputNotaAtividade.value));
+  if (atividades.includes(inputNomeAtividade.value)) {
+    alert(`A atividade ${inputNomeAtividade.value} já foi inserida!`);
+  } else {
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
 
-  let linha = "<tr>";
-  linha += `<td>${inputNomeAtividade.value}</td>`;
-  linha += `<td>${inputNotaAtividade.value}</td>`;
-  linha += `<td>${
-    inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado
-  }</td>`;
-  linha += "</tr>";
+    let linha = "<tr>";
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${
+      inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado
+    }</td>`;
+    linha += "</tr>";
 
-  linhas += linha;
+    linhas += linha;
+  }
 
   inputNomeAtividade.value = "";
   inputNotaAtividade.value = "";
@@ -47,7 +54,7 @@ function atualizaMediaFinal() {
 
   document.getElementById("media-final-valor").innerHTML = mediaFinal;
   document.getElementById("media-final-resultado").innerHTML =
-    mediaFinal >= 7 ? "Aprovado" : "Reprovado";
+    mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
 function calculaMediaFinal() {
